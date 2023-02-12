a99ebb2ae595f84c05b67d4a97d138a05e8b4b73
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-02-12 09:25:32 -0600

    feat(web): set up tailwind

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from 'ui'
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 className="text-3xl text-blue-500 font-bold">Web</h1>
       <Button />
     </div>
   )
