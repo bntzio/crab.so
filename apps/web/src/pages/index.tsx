ea733ec405894d6a05e685552463c892163023e8
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-02-13 17:15:13 -0600

    feat(web): hide elements when connected

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { UserGroupIcon } from '@heroicons/react/24/outline'
+import { useWallet } from '@solana/wallet-adapter-react'
 import { useWalletModal } from '@solana/wallet-adapter-react-ui'
 import { Button } from 'ui'
 
 import { Feed } from '@/components/Feed'
 
 export default function Index() {
+  const { connected } = useWallet()
   const { setVisible } = useWalletModal()
 
   return (
@@ -16,33 +18,37 @@ export default function Index() {
               🦀
             </span>
           </p>
-          <Button onClick={() => setVisible(true)}>
-            <UserGroupIcon className="h-5 w-5 text-white mr-2" aria-hidden="true" />
-            Create a community
-          </Button>
-        </div>
-        <div className="flex flex-col space-y-6 py-20">
-          <h1 className="text-xl font-medium leading-normal">
-            Crab is a community-driven, open-source, decentralized network for thriving communities in Web3
-          </h1>
-          <div className="flex flex-col items-start space-y-3">
-            <Button buttonType="slate" onClick={() => setVisible(true)}>
-              Join the network
+          {!connected && (
+            <Button onClick={() => setVisible(true)}>
+              <UserGroupIcon className="h-5 w-5 text-white mr-2" aria-hidden="true" />
+              Create a community
             </Button>
-            <p className="text-xs">
-              or{' '}
-              <a
-                href="#"
-                className="text-orange-400 hover:text-orange-500 border-b-2 border-orange-200 hover:border-orange-400"
-              >
-                learn more about it
-              </a>
-            </p>
-          </div>
+          )}
         </div>
+        {!connected && (
+          <div className="flex flex-col space-y-6 py-20">
+            <h1 className="text-xl font-medium leading-normal">
+              Crab is a community-driven, open-source, decentralized network for thriving communities in Web3
+            </h1>
+            <div className="flex flex-col items-start space-y-3">
+              <Button buttonType="slate" onClick={() => setVisible(true)}>
+                Join the network
+              </Button>
+              <p className="text-xs">
+                or{' '}
+                <a
+                  href="#"
+                  className="text-orange-400 hover:text-orange-500 border-b-2 border-orange-200 hover:border-orange-400"
+                >
+                  learn more about it
+                </a>
+              </p>
+            </div>
+          </div>
+        )}
       </section>
-      <div>
-        <div>
+      <div className={!connected ? 'mt-0' : 'mt-16'}>
+        <div className="mb-5">
           <p className="text-black font-medium items-center flex">
             <span aria-label="crab" role="img" className="mr-3">
               ✍️
