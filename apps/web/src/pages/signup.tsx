dbad600ae6aa17824852c65902e44a17ba00bd8f
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-02-18 11:33:20 -0600

    refactor(web): remove unused code

--- apps/web/src/pages/signup.tsx
+++ apps/web/src/pages/signup.tsx
@@ -1,18 +1,6 @@
-import { useWallet } from '@solana/wallet-adapter-react'
-import { useWalletModal } from '@solana/wallet-adapter-react-ui'
-import { useState } from 'react'
-
 import { SignupForm } from '@/components'
-import { useSplingStore } from '@/stores'
 
 export default function Signup() {
-  const wallet = useWallet()
-  const { setVisible } = useWalletModal()
-  const { getAllGroups, socialProtocol } = useSplingStore()
-  const [accountFound, setAccountFound] = useState<boolean | undefined>()
-
-  const { connected } = wallet
-
   return (
     <main className="py-8">
       <h1 className="text-xl font-medium">
