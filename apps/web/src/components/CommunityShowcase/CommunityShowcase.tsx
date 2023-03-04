088e6e62c593e04e89ff0d800c8afe42ab3b9a36
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-04 13:51:06 -0600

    feat(web): add community link

--- apps/web/src/components/CommunityShowcase/CommunityShowcase.tsx
+++ apps/web/src/components/CommunityShowcase/CommunityShowcase.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 const CommunityShowcase = () => {
@@ -21,26 +22,28 @@ const CommunityShowcase = () => {
 
   const renderCommunities = () => {
     return communities.map(community => (
-      <div className="flex flex-col justify-center items-center p-3 space-y-2" key={community.groupId}>
-        {community?.avatar ? (
-          <Image
-            src={community.avatar}
-            alt={community.name}
-            width={66}
-            height={66}
-            style={{ width: '66px', height: '66px' }}
-            className="rounded-full bg-cover bg-no-repeat"
-          />
-        ) : (
-          <div className="flex justify-center items-center w-[66px] h-[66px] bg-orange-600 text-white font-medium rounded-full">
-            {community.name[0].toUpperCase()}
+      <Link href={`/c/${community.metadata?.slug}`} key={community.groupId}>
+        <div className="flex flex-col justify-center items-center p-3 space-y-2">
+          {community?.avatar ? (
+            <Image
+              src={community.avatar}
+              alt={community.name}
+              width={66}
+              height={66}
+              style={{ width: '66px', height: '66px' }}
+              className="rounded-full bg-cover bg-no-repeat"
+            />
+          ) : (
+            <div className="flex justify-center items-center w-[66px] h-[66px] bg-orange-600 text-white font-medium rounded-full">
+              {community.name[0].toUpperCase()}
+            </div>
+          )}
+          <div className="space-y-1 flex flex-col items-center">
+            <h4 className="text-gray-500 font-normal">{community.name}</h4>
+            <p className="text-gray-400 text-xs">117 members</p>
           </div>
-        )}
-        <div className="space-y-1 flex flex-col items-center">
-          <h4 className="text-gray-500 font-normal">{community.name}</h4>
-          <p className="text-gray-400 text-xs">117 members</p>
         </div>
-      </div>
+      </Link>
     ))
   }
 
