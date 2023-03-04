339dedd2181fc43a0d60375d1abd8ea6269a037c
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-04 13:47:18 -0600

    chore(web): update database seed

--- apps/web/supabase/seed.sql
+++ apps/web/supabase/seed.sql
@@ -5,5 +5,6 @@ values
   ('Lisbon', 'BWPsHYwDftJ44XK2opLrXRfqeimoaX9dGZJRYkeRCcbq'),
   ('Cute dogs', 'Dyuc7gXWDUCFZqgNX47EDiXkiE9TiXfBoKXNuFCGgiNo'),
   ('Movies', 'D1oXVJRwSf3SEHxPxEVnoCeqFwj3penauzmJVYJgftha'),
-  ('Weight', '7mrpXGKobkAqG4VjQMCSWCbsJbbxAFXWDwkefjhYPmot'),
-  ('Yoga', '5rPGtbji1vhozoeXEDeJSYLVU7BqozoDSwRar8SPuyTL');
+  ('Shadow Storage', 'DmU3zbr6re1oN4J4E7gDhf7FWPF7vf47D1G1j5qvTE9i'),
+  ('Yoga', '5rPGtbji1vhozoeXEDeJSYLVU7BqozoDSwRar8SPuyTL'),
+  ('Weight', '7mrpXGKobkAqG4VjQMCSWCbsJbbxAFXWDwkefjhYPmot');
