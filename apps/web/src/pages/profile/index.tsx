63c574814b7f4196379925e93dac2d263c6e73a7
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-04 19:58:25 -0600

    feat(web): delete users

--- apps/web/src/pages/profile/index.tsx
+++ apps/web/src/pages/profile/index.tsx
@@ -1,5 +1,5 @@
 import { useWallet } from '@solana/wallet-adapter-react'
-import { Group } from '@spling/social-protocol'
+import { Group, User } from '@spling/social-protocol'
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 
@@ -8,49 +8,92 @@ import { useSplingStore } from '@/stores'
 export default function Profile() {
   const wallet = useWallet()
   const router = useRouter()
-  const [groups, setGroups] = useState<Group[]>([])
   const { getAllGroups, socialProtocol } = useSplingStore()
+  // TODO: Manage state in a store
+  const [user, setUser] = useState<User>()
+  const [communities, setCommunities] = useState<Group[]>()
 
   useEffect(() => {
-    async function fetchProtocolInfo() {
-      setTimeout(async () => {
-        const allGroups = await getAllGroups()
+    async function fetchUser() {
+      if (!wallet.publicKey) return
 
-        const crabGroups = allGroups.filter(group => group.metadata?.slug)
+      const user = await socialProtocol?.getUserByPublicKey(wallet.publicKey)
 
-        setGroups(crabGroups)
-
-        if (wallet?.publicKey) {
-          const user = await socialProtocol?.getUserByPublicKey(wallet.publicKey)
-          console.log('my user', user)
-        }
-      }, 1200)
+      if (user) setUser(user)
     }
 
-    fetchProtocolInfo()
-  }, [wallet, socialProtocol, getAllGroups])
+    if (!user) fetchUser()
+  }, [socialProtocol, wallet, user])
 
-  const handleDelete = async () => {
-    await socialProtocol?.deleteGroup()
-  }
+  useEffect(() => {
+    async function fetchCommunities() {
+      const response = await fetch('/api/communities')
+      const data = await response.json()
+
+      setCommunities(data)
+    }
+
+    fetchCommunities()
+  }, [])
+
+  if (!user || !communities?.length) return <p>Loading...</p>
+
+  const joinedGroupIds = user.groups
+
+  const joinedCommunities = communities.filter(community => {
+    return joinedGroupIds.includes(community.groupId)
+  })
 
   return (
     <section className="mt-10">
-      <h1 className="text-xl font-bold mb-6">Profile</h1>
-      <h3 className="text-lg font-semibold mb-3">Your communities</h3>
+      <h1 className="text-xl font-bold mb-6">My profile</h1>
+
+      <div>
+        <button
+          className="bg-red-500 text-white px-2 rounded-sm"
+          onClick={async () => await socialProtocol?.deleteGroup()}
+        >
+          Delete my community
+        </button>
+      </div>
+
+      <div className="mt-4">
+        <button
+          className="bg-red-500 text-white px-2 rounded-sm"
+          onClick={async () => await socialProtocol?.deleteUser()}
+        >
+          Delete my account
+        </button>
+      </div>
+
+      <h3 className="text-lg font-semibold mb-3 mt-6">All Crab communities:</h3>
       <ul className="space-y-3">
-        {groups.map(group => (
+        {communities.map(community => (
           <li className="space-x-4 flex">
-            <span>{group.name}</span>
+            <span>{community.name}</span>
             <div className="space-x-2">
               <button
                 className="bg-blue-500 text-white px-2 rounded-sm"
-                onClick={() => router.push(`/c/${group.metadata?.slug}`)}
+                onClick={() => router.push(`/c/${community.metadata?.slug}`)}
               >
                 View
               </button>
-              <button className="bg-red-500 text-white px-2 rounded-sm" onClick={handleDelete}>
-                Delete
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <h3 className="text-lg font-semibold mb-3 mt-10">Joined to the following Crab communities:</h3>
+      <ul className="space-y-3">
+        {joinedCommunities.map(joinedCommunity => (
+          <li className="space-x-4 flex">
+            <span>{joinedCommunity.name}</span>
+            <div className="space-x-2">
+              <button
+                className="bg-blue-500 text-white px-2 rounded-sm"
+                onClick={() => router.push(`/c/${joinedCommunity.metadata?.slug}`)}
+              >
+                View
               </button>
             </div>
           </li>
