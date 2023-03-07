2a3b56e6e13d86734a08a62def975b5845288e5f
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-06 19:36:53 -0600

    feat(web): set commitment level to confirmed

--- apps/web/src/components/WalletAdapter/WalletAdapter.tsx
+++ apps/web/src/components/WalletAdapter/WalletAdapter.tsx
@@ -15,7 +15,7 @@ const WalletAdapter = ({ children }: Props) => {
   const wallets = useMemo(() => [new GlowWalletAdapter(), new SolflareWalletAdapter()], [])
 
   return (
-    <ConnectionProvider endpoint={endpoint} config={{ commitment: 'max' }}>
+    <ConnectionProvider endpoint={endpoint} config={{ commitment: 'confirmed' }}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
