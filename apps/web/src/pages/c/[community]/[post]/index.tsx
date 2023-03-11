d5806ccd97d515a7d32b50b759c6868cb74ab410
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-10 21:38:40 -0600

    feat(web): render full post info

--- apps/web/src/pages/c/[community]/[post]/index.tsx
+++ apps/web/src/pages/c/[community]/[post]/index.tsx
@@ -1,22 +1,37 @@
+import { PublicKey } from '@solana/web3.js'
+import { Post } from '@spling/social-protocol'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 import { Button } from 'ui'
 
-export default function Post() {
+import { useSplingStore } from '@/stores'
+
+export default function PostPage() {
   const router = useRouter()
+  const { socialProtocol } = useSplingStore()
+  const [post, setPost] = useState<Post | null>(null)
 
-  const { community, post } = router.query
+  const { community } = router.query
 
-  console.log(post)
+  useEffect(() => {
+    async function init() {
+      if (!router.query.post || typeof router.query.post !== 'string') return
+
+      const postResult = await socialProtocol?.getPostByPublicKey(new PublicKey(router.query.post))
+
+      if (postResult) setPost(postResult)
+    }
+
+    init()
+  }, [socialProtocol, router.query.post])
+
+  if (!post) return <div>Loading...</div>
 
   return (
     <main>
-      <h1 className="text-xl font-medium">Title Here</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, nisl nec ultricies lacinia, nisl nisl
-        aliquet nisl, nec tincidunt nisl nisl et nisl. Sed tincidunt, nisl nec ultricies lacinia, nisl nisl aliquet
-        nisl, nec tincidunt nisl nisl et nisl.
-      </p>
+      <h1 className="text-xl font-medium">{post.title}</h1>
+      <p>{post.text}</p>
 
       <div className="mt-6">
         <Button buttonType="slate">
