7fc8dc6d8c209da2a47096d43c517542a9024609
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-11 08:49:45 -0600

    feat(web): vote post on post page

--- apps/web/src/pages/c/[community]/[post]/index.tsx
+++ apps/web/src/pages/c/[community]/[post]/index.tsx
@@ -1,3 +1,4 @@
+import { ChevronUpIcon } from '@heroicons/react/24/solid'
 import { PublicKey } from '@solana/web3.js'
 import { Post, Reply } from '@spling/social-protocol'
 import Link from 'next/link'
@@ -6,15 +7,25 @@ import { useState, useEffect } from 'react'
 import { Button } from 'ui'
 
 import { ReplyForm } from '@/components'
-import { useSplingStore } from '@/stores'
+import { useSplingStore, useUserStore } from '@/stores'
 
 export default function PostPage() {
   const router = useRouter()
+  const { user } = useUserStore()
   const { socialProtocol } = useSplingStore()
   const [post, setPost] = useState<Post | null>(null)
   const [replies, setReplies] = useState<Reply[] | null>(null)
+  const [isUpvoted, setIsUpvoted] = useState(false)
+  const [votes, setVotes] = useState<number | null>(null)
 
   const { community } = router.query
+
+  useEffect(() => {
+    if (user && post) {
+      const hasLiked = post.likes.includes(user.userId)
+      setIsUpvoted(hasLiked)
+    }
+  }, [user, post])
 
   useEffect(() => {
     async function init() {
@@ -22,7 +33,10 @@ export default function PostPage() {
 
       const postResult = await socialProtocol?.getPostByPublicKey(new PublicKey(router.query.post))
 
-      if (postResult) setPost(postResult)
+      if (postResult) {
+        setPost(postResult)
+        setVotes(postResult.likes.length)
+      }
     }
 
     init()
@@ -42,6 +56,23 @@ export default function PostPage() {
     getReplies()
   }, [post, socialProtocol])
 
+  const handleVoteDownvote = async (ev: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    ev.stopPropagation()
+
+    if (!post || votes === null) return
+
+    try {
+      await socialProtocol?.likePost(post.publicKey)
+
+      if (isUpvoted) setVotes(votes - 1)
+      else setVotes(votes + 1)
+
+      setIsUpvoted(!isUpvoted)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   const renderReplies = () => {
     if (replies === null) return <div>Loading comments...</div>
     if (replies?.length === 0) return <div>There are no comments.</div>
@@ -54,7 +85,7 @@ export default function PostPage() {
           src={reply.user.avatar || '/images/0xPegasus.png'}
           alt={`${reply.user.nickname} avatar`}
         />
-        <div className="flex flex-col space-y-2 justify-center">
+        <div className="flex flex-col space-y-1 justify-center">
           <div className="flex items-center text-xs space-x-1">
             <p className="font-medium">{reply.user.nickname}</p>
             <time>at {reply.timestamp}</time>
@@ -80,8 +111,27 @@ export default function PostPage() {
       <br />
       <br />
 
-      <h1 className="text-xl font-medium">{post.title}</h1>
-      <p>{post.text}</p>
+      <div className="flex items-start space-x-4">
+        <div className="flex justify-between space-x-3 items-center">
+          <div className="-mt-3">
+            <div className="rounded-full p-[2px]" onClick={handleVoteDownvote}>
+              <span className="sr-only">Upvote</span>
+              <ChevronUpIcon
+                className={`h-5 w-5 ${isUpvoted ? 'text-orange-400' : 'text-gray-400'}`}
+                aria-hidden="true"
+              />
+            </div>
+            <div className="flex justify-center">
+              {votes && <span className="text-xs font-medium text-gray-500 py-1">{votes}</span>}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col space-y-4">
+          <h1 className="text-xl font-medium">{post.title}</h1>
+          <p>{post.text}</p>
+        </div>
+      </div>
 
       <br />
       <br />
