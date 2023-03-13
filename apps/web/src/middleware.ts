4c085481cb1f4826d2a7420f51558cf627c75f1c
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-13 17:24:06 -0600

    feat(web): protect home route

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,21 +1,40 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 const heliusApiKey = process.env.HELIUS_API_KEY
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl
 
   if (pathname === '/api/rpc') {
-    const url = new URL('https://rpc.helius.xyz', request.url)
+    const url = new URL('https://rpc.helius.xyz', req.url)
 
     if (heliusApiKey) url.searchParams.set('api-key', heliusApiKey)
 
-    const response = NextResponse.rewrite(url)
+    const res = NextResponse.rewrite(url)
 
-    return response
+    return res
+  } else {
+    const res = NextResponse.next()
+
+    const supabase = createMiddlewareSupabaseClient({ req, res })
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession()
+
+    if (session) return res
+
+    const redirectUrl = req.nextUrl.clone()
+
+    redirectUrl.pathname = '/'
+    redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
+
+    return NextResponse.redirect(redirectUrl)
   }
 }
 
 export const config = {
-  matcher: ['/api/rpc'],
+  matcher: ['/api/rpc', '/home'],
 }
