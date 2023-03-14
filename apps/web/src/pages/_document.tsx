7038ffe34d44ec19bb4db5eb25ba742e793afebb
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-14 14:58:45 -0600

    feat(web): remove document styles

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className="h-full bg-gray-50">
+    <Html lang="en">
       <Head />
       <body className="h-full">
         <Main />
