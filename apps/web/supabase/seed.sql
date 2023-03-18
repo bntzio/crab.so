0c79af55dc99316ea3045b1534f8c4936c45cdb0
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-18 16:38:55 -0600

    fix(web): update database seed

--- apps/web/supabase/seed.sql
+++ apps/web/supabase/seed.sql
@@ -1,4 +1,9 @@
-insert into public.communities
-  (name, public_key)
+insert into auth.users
+  (id, email)
 values
-  ('Shadow Storage', 'DmU3zbr6re1oN4J4E7gDhf7FWPF7vf47D1G1j5qvTE9i');
+  ('cc898a26-1b71-4ed0-b567-63e18776cb45', 'shaggy@email.com');
+
+insert into public.communities
+  (name, public_key, owner, slug)
+values
+  ('Ghosts', 'FhFUg8VTf5fq97X3q8Cr7yRFAbFGCm2czFhx1WxD92yK', 'cc898a26-1b71-4ed0-b567-63e18776cb45', 'ghosts');
