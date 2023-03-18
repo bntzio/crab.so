e7507453150f12f41660e35d0def84f5620098d8
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-17 18:56:35 -0600

    chore(web): update database seed

--- apps/web/supabase/seed.sql
+++ apps/web/supabase/seed.sql
@@ -1,10 +1,4 @@
 insert into public.communities
   (name, public_key)
 values
-  ('Breakpoint', '5UBEeQqPY1jkJm7rfW9xR1PwJwHuoz4C6UqTsAge4YqA'),
-  ('Lisbon', 'BWPsHYwDftJ44XK2opLrXRfqeimoaX9dGZJRYkeRCcbq'),
-  ('Cute dogs', 'Dyuc7gXWDUCFZqgNX47EDiXkiE9TiXfBoKXNuFCGgiNo'),
-  ('Movies', 'D1oXVJRwSf3SEHxPxEVnoCeqFwj3penauzmJVYJgftha'),
   ('Shadow Storage', 'DmU3zbr6re1oN4J4E7gDhf7FWPF7vf47D1G1j5qvTE9i'),
-  ('Yoga', '5rPGtbji1vhozoeXEDeJSYLVU7BqozoDSwRar8SPuyTL'),
-  ('Weight', '7mrpXGKobkAqG4VjQMCSWCbsJbbxAFXWDwkefjhYPmot');
