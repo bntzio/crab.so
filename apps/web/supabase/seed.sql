9b24797f2cfc859573f751fe168604802697c0b2
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-03-17 22:35:13 -0600

    fix(web): seed sql syntax

--- apps/web/supabase/seed.sql
+++ apps/web/supabase/seed.sql
@@ -1,4 +1,4 @@
 insert into public.communities
   (name, public_key)
 values
-  ('Shadow Storage', 'DmU3zbr6re1oN4J4E7gDhf7FWPF7vf47D1G1j5qvTE9i'),
+  ('Shadow Storage', 'DmU3zbr6re1oN4J4E7gDhf7FWPF7vf47D1G1j5qvTE9i');
